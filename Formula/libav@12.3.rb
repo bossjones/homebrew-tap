4c1c9979aecbb342092f4dcc2226f7e891dc7e83
Malcolm Jones <bossjones@theblacktonystark.com>
2023-09-22 14:39:04 -0400

    feat: initial bottles

--- Formula/libav@12.3.rb
+++ Formula/libav@12.3.rb
@@ -9,6 +9,15 @@ class LibavAT123 < Formula
   revision 1
   head "https://git.libav.org/libav.git", branch: "master"
 
+  # # The homepage for this formula is a copy of the original and was created
+  # # after the original was discontinued. There will be no further releases of
+  # # the copy of this software used in the formula, as the developer is deceased.
+  # # New development of libagg occurs in a fork of v2.4 and can be found at:
+  # # https://sourceforge.net/projects/agg/
+  # livecheck do
+  #   skip "No longer developed/maintained"
+  # end
+
   depends_on "pkg-config" => :build
   # manpages won't be built without texi2html
   depends_on "texi2html" => :build
